e21007a0651b12aff6d18062bc24add2e411ed45
eric belfer <esbelf@users.noreply.github.com>
2022-09-01 10:06:53 +0200

    WIP

--- src/components/order/index.js
+++ src/components/order/index.js
@@ -1,3 +1,4 @@
 export { default as AddressForm } from "./AddressForm";
 export { default as ContactDetailForm } from "./ContactDetailForm";
 export { default as ProductForm } from "./ProductForm";
+export { default as StripeCreditCardCheckout } from "./StripeCreditCardCheckout";
