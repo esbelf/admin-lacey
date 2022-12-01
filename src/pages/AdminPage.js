42493f435f17426ca8cdd4c977b1c261931334c0
eric belfer <esbelf@users.noreply.github.com>
2022-12-01 10:51:24 -0300

    update

--- src/pages/AdminPage.js
+++ src/pages/AdminPage.js
@@ -21,6 +21,9 @@ export default function AdminPage() {
       <Paper className="my-6">
         <MatTypeTotals />
       </Paper>
+      <Paper className="my-6">
+        <StripeMonthTotals />
+      </Paper>
     </Wrapper>
   );
 }
@@ -96,6 +99,57 @@ function MatTypeTotals() {
         // argumentField=""
         // pointComponent
       />
+    </Chart>
+  );
+}
+
+function StripeMonthTotals() {
+  const { data, loading } = useApiFetch({
+    url: `/meta_data/stripe_month_totals`,
+  });
+  if (loading) {
+    return (
+      <Wrapper>
+        <p>Loading</p>
+      </Wrapper>
+    );
+  }
+  console.log("StripeMonthTotals", data);
+  const month = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const monthTotal = data
+    .map((row) => {
+      const date = new Date(row.month);
+      const total = row.stripeCharge - row.stripeFee; // - row.stripeRefund;
+      return {
+        total: total * 0.01,
+        month: month[date.getMonth()],
+        monthInt: date.getMonth(),
+      };
+    })
+    .sort((a, b) => a.monthInt - b.monthInt);
+  const currentYear = new Date().getFullYear();
+  return (
+    <Chart data={monthTotal} size={{ width: "100%", height: "75%" }}>
+      <ArgumentAxis />
+      <ValueAxis />
+      <BarSeries valueField="total" argumentField="month" />
+      <Title
+        text={`Stripe total by month for ${currentYear} (refund not accounted for currently)`}
+      />
+      <Animation name="Stripe total by month" />
     </Chart>
   );
 }
